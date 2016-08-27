02debfa49a0923e2cd8714ebeee33747244fbfaf
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-08-26 18:04:44 -0700

    Add a working solution for the Sum of Integers prompt

--- src/recursion.js
+++ src/recursion.js
@@ -20,7 +20,12 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
-
+  if (array.length === 1) {
+    return array[0];
+  }
+  while (array.length > 0) {
+    return array[array.length-1] + sum(array.slice(0,array.length-1));
+  }
 };
 
 
